b78abc24536fda685c45f71c4a6cbaf06c851679
AnatoliiWorkmail <jp.komod.su@gmail.com>
2022-04-06 23:10:28 +0300

    add-value

--- src/index.js
+++ src/index.js
@@ -5,16 +5,28 @@ const DEBOUNCE_DELAY = 300;
 const ref = {
   input: document.querySelector('#search-box'),
   list: document.querySelector('.country-list'),
-  info: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
 };
+
 ref.input.addEventListener(
   'input',
   debounce(() => {
-    const searchName = ref.input.value;
-    console.log(ref.input.value);
-  }, 300),
+    const searchName = ref.input.value.trim();
+
+    fetchCountries(searchName);
+  }, DEBOUNCE_DELAY),
 );
-// function fetchCountries(name) {}
-// https://restcountries.com/v2/name/{name}
-// .trim()
+
+//return fetch('https://restcountries.com/v2/name/sweden');
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v2/name/${name}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(renderMarkup);
+}
+function renderMarkup(name) {
+  return name.map(({ name }) => (ref.list.innerHTML = `<li>${name}</li>`));
+}
+
 // trow Error
