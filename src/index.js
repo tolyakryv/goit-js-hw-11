20415dab5be643f00022c5ad41312624bf3f91c8
AnatoliiWorkmail <jp.komod.su@gmail.com>
2022-04-06 21:11:43 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const ref = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-list'),
+};
+ref.input.addEventListener(
+  'input',
+  debounce(() => {
+    const searchName = ref.input.value;
+    console.log(ref.input.value);
+  }, 300),
+);
+// function fetchCountries(name) {}
+// https://restcountries.com/v2/name/{name}
+// .trim()
+// trow Error
