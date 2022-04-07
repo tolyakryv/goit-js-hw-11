e329b24779318c9d4501b7c930524d6c10c3fea0
AnatoliiWorkmail <jp.komod.su@gmail.com>
2022-04-07 17:37:48 +0300

    add-flag

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
@@ -17,7 +18,6 @@ ref.input.addEventListener(
   }, DEBOUNCE_DELAY),
 );
 
-//return fetch('https://restcountries.com/v2/name/sweden');
 function fetchCountries(name) {
   return fetch(`https://restcountries.com/v2/name/${name}`)
     .then(response => {
@@ -26,7 +26,39 @@ function fetchCountries(name) {
     .then(renderMarkup);
 }
 function renderMarkup(name) {
-  return name.map(({ name }) => (ref.list.innerHTML = `<li>${name}</li>`));
+  ref.list.innerHTML = '';
+  ref.info.innerHTML = '';
+  if (name.length == null) {
+    Notify.failure('not found');
+  }
+  const markupCountriesInfo = name
+    .map(
+      ({ name, capital, flag, population, languages }) =>
+        `<ul><li><img src='${flag}' alt ="${name}"  width="30" height="15" ></img>   ${name}</li>
+      <li>Capital: ${capital}</li>
+      <li>Population: ${population}</li>
+      <li>Languages:${languages[0].name}</li></ul>`,
+    )
+    .join('');
+  const markupCountriesList = name
+    .map(
+      ({ name, flag }) =>
+        `<li><img src='${flag}' alt ="${name}"  width="30" height="15" ></img>   ${name}</li>`,
+    )
+    .join('');
+  if (name.length === 1) {
+    ref.info.insertAdjacentHTML('afterbegin', markupCountriesInfo);
+  } else {
+    if (name.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+      return console.log(name.length);
+    }
+    ref.list.insertAdjacentHTML('afterbegin', markupCountriesList);
+  }
 }
 
 // trow Error
+// .catch(reject => {
+//       console.log(reject);
+//       return Notify.failure('qqqqqqqqqq');
+//     })
